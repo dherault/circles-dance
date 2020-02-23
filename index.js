715a375833e231cfdfd8ec96eb9396de824cff82
David Hérault <dherault@gmail.com>
2020-02-23 18:37:30 +0100

    First steps

---
+++ index.js
@@ -0,0 +1,169 @@
+const { cos, sin, PI, pow, abs } = Math
+const TAU = 2 * PI
+const canvas = document.getElementsByTagName('canvas')[0]
+const _ = canvas.getContext('2d')
+
+const width = canvas.width = window.innerWidth
+const height = canvas.height = window.innerHeight
+
+/* ---
+  Data
+--- */
+
+const startingRho = 64
+
+const data = [
+  createCirclesStructure(squareWave),
+  createCirclesStructure(triangleWave),
+  createCirclesStructure(squareWave),
+]
+
+function createCirclesStructure(fn) {
+  return {
+    circles: fn(),
+    points: []
+  }
+}
+
+function squareWave() {
+  const circles = []
+
+  for (let i = 1; i < 126 + 1; i += 2) {
+    circles.push({
+      t: PI,
+      r: startingRho / i,
+      a: i,
+    })
+  }
+
+  return circles
+}
+
+function triangleWave() {
+  const circles = []
+
+  for (let i = 1; i < 126 + 1; i += 2) {
+    const n = 2 * i + 1
+
+    circles.push({
+      t: PI,
+      r: startingRho * pow(-1, i) / (n * n),
+      a: n,
+    })
+  }
+  return circles
+}
+
+/* ---
+  Update
+--- */
+
+function update() {
+  data.forEach(({ circles, points }) => {
+    circles.forEach(c => {
+      c.t += c.a / 100
+    })
+
+    points.forEach((p, i) => {
+      p.x += 0.5
+
+      if (p.x > width + 1) {
+        points.splice(i, 1)
+      }
+    })
+  })
+}
+
+/* ---
+  Draw
+--- */
+
+function draw() {
+  _.fillStyle = 'white'
+  _.strokeStyle = 'black'
+  _.fillRect(0, 0, width, height)
+
+  data.forEach(({ circles, points }, k) => {
+    let previousCoordinates = { x: width / 3, y: (k + 1) * height / (data.length + 1) }
+
+    circles.forEach(({ t, r }) => {
+      const nextCoordinates = sumPoints(previousCoordinates, polarToCarthesian(t, r))
+
+      drawCircle(previousCoordinates.x, previousCoordinates.y, r, nextCoordinates)
+
+      previousCoordinates = nextCoordinates
+    })
+
+    const point = { x: 2 * width / 3, y: previousCoordinates.y }
+
+    _.moveTo(previousCoordinates.x, previousCoordinates.y)
+    _.lineTo(point.x, point.y)
+    _.stroke()
+
+    points.push(point)
+
+    points.forEach((p, i) => {
+
+      if (i + 1 < points.length && distanceSquared(p, points[i + 1]) > 1) {
+        _.moveTo(p.x, p.y)
+        _.lineTo(points[i + 1].x, points[i + 1].y)
+        _.stroke()
+      }
+      else {
+        drawPoint(p.x, p.y)
+      }
+    })
+  })
+}
+
+function drawCircle(x, y, r, p) {
+  _.strokeStyle = 'black'
+  _.save()
+  // _.translate(x, y)
+  _.beginPath()
+  _.arc(x, y, abs(r), 0, TAU)
+  _.closePath()
+  _.stroke()
+  _.moveTo(x, y)
+  _.lineTo(p.x, p.y)
+  _.stroke()
+}
+
+function drawPoint(x, y) {
+  _.fillStyle = 'black'
+  _.beginPath()
+  _.arc(x, y, 0.5, 0, TAU)
+  _.closePath()
+  _.fill()
+}
+
+function tick() {
+  update()
+  draw()
+
+  window.requestAnimationFrame(tick)
+}
+
+/* ---
+  Methods
+--- */
+
+function sumPoints(a, b) {
+  return {
+    x: a.x + b.x,
+    y: a.y + b.y,
+  }
+}
+
+function polarToCarthesian(theta, rho) {
+  return {
+    x: rho * cos(theta),
+    y: rho * sin(theta),
+  }
+}
+
+function distanceSquared(a, b) {
+  return (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)
+}
+
+window.requestAnimationFrame(tick)
